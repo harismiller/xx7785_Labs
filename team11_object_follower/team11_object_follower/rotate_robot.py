1b3d1d8deee48cfe87cf49c5fe031706cffd4c4e
Haris Miller <hm-tested@users.noreply.github.com>
2023-09-28 16:45:14 -0400

    Add files via upload

--- team11_object_follower/team11_object_follower/rotate_robot.py
+++ team11_object_follower/team11_object_follower/rotate_robot.py
@@ -2,12 +2,12 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSReliabilityPolicy, QoSHistoryPolicy
-from geometry_msgs.msg import Point,Twist
+from geometry_msgs.msg import Point,Twist,Vector3
 
 import sys
 
 import numpy as np
-import introcs
+
 
 class RobotRotation(Node):
 
@@ -27,9 +27,9 @@ class RobotRotation(Node):
     def _rotation_callback(self, Point):
         msg = Twist()
         if Point.x > 0:
-            msg.angular = introcs.Vector3(5,0,0)
+            msg.angular = Vector3(5,0,0)
         elif Point.x < 0:
-            msg.angular = introcs.Vector3(-5,0,0)
+            msg.angular = Vector3(-5,0,0)
         msg.angular
         self._vel_publish.publish(msg)
 
