465469a5616cebfa3847cc304bfe20959899b33c
Haris Miller <19560137+harismiller@users.noreply.github.com>
2023-10-26 20:37:09 -0400

    To test getToGoal

--- team11_navigate_to_goal/team11_navigate_to_goal/goToGoal.py
+++ team11_navigate_to_goal/team11_navigate_to_goal/goToGoal.py
@@ -40,47 +40,52 @@ class MoveRobot(Node):
                 5)
         
     def _move_callback(self, point):
-        goal = self.waypts[self.current_wp_index]
-        #determine waypoint based on your algo
-        curr_pos = [self.odom.x,self.odom.y]
-        dist_to_goal = np.sqrt((curr_pos[0]-goal[0])**2+(curr_pos[1]-goal[1])**2)
-        if dist_to_goal>self.dstar:
-            xa,ya = -1*self.dstar*self.zeta*(curr_pos-goal)/dist_to_goal
-        else:
-            xa,ya = -5.0*self.zeta*(curr_pos-goal)
-        xr = []
-        yr = []
-        dist_to_obstacle,closest_point = point.z,[point.x,point.y]
-        if dist_to_obstacle<self.qstar:
-            # print('ons_in_con')
-            coeff = -self.neta*(dist_to_obstacle-self.qstar)/((dist_to_obstacle**4)*self.qstar)
-            # print(coeff,'coef')
-            xrg,yrg = coeff*(curr_pos-closest_point)
-        else:
-            xrg,yrg = 0,0
-        xr.append(xrg)
-        yr.append(yrg)
-        xr = np.array(xr)
-        yr = np.array(yr)
-        xut = xa+np.sum(xr)
-        yut = ya+np.sum(yr)
-        xu = xut/np.sqrt(xut**2+yut**2)
-        yu = yut/np.sqrt(xut**2+yut**2)
-        if dist_to_goal<self.reached_threshold:
-            self.current_wp_index+=1
-
-
-
         msg = Twist()
-        # msg.x = pos
+        if self.current_wp_index > len(self.waypts):
+            goal = self.waypts[self.current_wp_index]
+            #determine waypoint based on your algo
+            curr_pos = [self.odom.x,self.odom.y]
+            dist_to_goal = np.sqrt((curr_pos[0]-goal[0])**2+(curr_pos[1]-goal[1])**2)
+            if dist_to_goal>self.dstar:
+                xa,ya = -1*self.dstar*self.zeta*(curr_pos-goal)/dist_to_goal
+            else:
+                xa,ya = -5.0*self.zeta*(curr_pos-goal)
+            xr = []
+            yr = []
+            dist_to_obstacle,closest_point = point.z,[point.x,point.y]
+            if dist_to_obstacle<self.qstar:
+                # print('ons_in_con')
+                coeff = -self.neta*(dist_to_obstacle-self.qstar)/((dist_to_obstacle**4)*self.qstar)
+                # print(coeff,'coef')
+                xrg,yrg = coeff*(curr_pos-closest_point)
+            else:
+                xrg,yrg = 0,0
+            xr.append(xrg)
+            yr.append(yrg)
+            xr = np.array(xr)
+            yr = np.array(yr)
+            xut = xa+np.sum(xr)
+            yut = ya+np.sum(yr)
+            xu = xut/np.sqrt(xut**2+yut**2)
+            yu = yut/np.sqrt(xut**2+yut**2)
+
+            xpos = curr_pos[0]
+            ypos = curr_pos[0]
+            lin_vel = (xpos*xu + ypos*yu)/np.sqrt(xpos**2 + ypos**2)
+            msg.linear.x = lin_vel*0.2
+            ang_vel = (xpos*yu - ypos*xu)/(xpos**2 + ypos**2)
+            msg.angular.z = ang_vel*1.5
+
+            if dist_to_goal<self.reached_threshold:
+                self.current_wp_index+=1
+        else:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+
         self._move_publish.publish(msg)
 
     def _odom_callback(self, odom):
         self.odom = odom
-
-    def cartToRadVel(xvel,yvel):
-        ang = math.atan2(yvel,xvel)
-        return ang
 
 def main():
     rclpy.init()
