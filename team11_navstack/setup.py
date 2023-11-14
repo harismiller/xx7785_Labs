9294e3f1512e14eae6ce40f19d682c31735acbc7
Haris Miller <19560137+harismiller@users.noreply.github.com>
2023-11-14 18:11:49 -0500

    Add navstack package

---
+++ team11_navstack/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'team11_navstack'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='haris',
+    maintainer_email='19560137+harismiller@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'send_point = team11_navstack.navigate_to_goal:main'
+        ],
+    },
+)
