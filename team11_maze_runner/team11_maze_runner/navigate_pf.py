a8934ec284f4de382a7b18fbd19f98308a285e35
Haris Miller <19560137+harismiller@users.noreply.github.com>
2023-12-03 01:31:30 -0500

    First navigate_pf

--- team11_maze_runner/team11_maze_runner/navigate_pf.py
+++ team11_maze_runner/team11_maze_runner/navigate_pf.py
@@ -13,20 +13,25 @@ class MoveRobot(Node):
         time.sleep(2)
         #Variables
         self.odom = Pose2D()
-        # self.waypts = np.array([[1.7, 0],[1.75, 1.5],[0.1, 1.6]])
+        # self.waypts = np.array([[1.7, 0]])
         self.dstar = 0.2
         self.qstar = 0.4
         self.zeta = 1
         self.neta = 0.01
         self.reached_threshold = 0.05
-        self.current_wp_index = 0
+        # self.current_wp_index = 0
+        self.goal_add = [0.0,0.0]
         self.ang_thresh_active = False
+        self.turn_goal_thresh = False
+        self.turn_thresh = False
+        self.dist_check = 0.0
 
         self.MAX_LIN = 0.2
         self.MAX_ANG = 1.5
         self.ANGLE_THRESHOLD = math.radians(170)
-        self.MIN_DIST_ANG_THRESH = 0.4
-        self.wait_count = 20
+        self.MIN_DIST = 0.5
+        # self.MIN_DIST_ANG_THRESH = 0.4
+        # self.wait_count = 20
 
         super().__init__('move_robot')
 
@@ -37,11 +42,16 @@ class MoveRobot(Node):
                 10)
         
         self._obj_subcriber = self.create_subscription(
-                Point,
+                Pose2D,
                 '/wallLocation',
                 self._move_callback,
                 10)
-
+        self._sign_subscriber = self.create_subscription(
+            Point,
+            '/sign',
+            self._sign_callback,
+            10
+        )
         self._move_publish = self.create_publisher(
                 Twist,
                 '/cmd_vel',
@@ -56,39 +66,71 @@ class MoveRobot(Node):
         v1_n = self.normalize_vector(v1)
         v2_n = self.normalize_vector(v2)
         return np.arccos(np.clip(np.dot(v1_n, v2_n), -1.0, 1.0)),v1_n,v2_n
+    
+    def transform_to_global(self,vec,orient):
+        local_to_global = np.array([[np.cos(orient),-np.sin(orient)],
+                           [np.sin(orient), np.cos(orient)]])
+        return local_to_global.dot(vec)
         
     def _sign_callback(self,sign):
         self.sign_type = int(sign.x)
         if self.sign_type == 0:
-            self.sign_value = 0
+            self.sign_value = [0,-0.3]
         elif self.sign_type == 1:
-            self.sign_value = np.deg2rad(87)
+            self.sign_value = [0.0,0.3]
         elif self.sign_type == 2:
-            self.sign_value = -np.deg2rad(87)
+            self.sign_value = [0.0,-0.3]
         elif self.sign_type == 3 or self.sign_type == 4:
-            self.sign_value = np.deg2rad(174)
+            self.sign_value = [-0.5,0.0]
         else:
-            self.sign_value = 0
+            self.sign_value = [0.0,0.0]
     
     def _move_callback(self, point):
         msg = Twist()
         #determine waypoint based on your algo
         curr_pos = [self.odom.x,self.odom.y]
-        goal = self.waypts[self.current_wp_index]
+        orientation = self.odom.theta
+
+        dist_to_obstacle = point.x
+        angle_to_obstacle = point.theta
+
+        closest_point_local = np.array([dist_to_obstacle*np.cos(angle_to_obstacle),
+                                        dist_to_obstacle*np.sin(angle_to_obstacle)])
+        closest_point_off = self.transform_to_global(closest_point_local,orientation)
+        closest_point = np.array([curr_pos[0]+closest_point_off[0],curr_pos[1]+closest_point_off[1]])
+
+        e_lin = dist_to_obstacle - self.MIN_DIST
+        if e_lin < 0.01:
+            print('check sign')
+            if self.turn_goal_thresh==False:
+                self.goal_add = np.array(self.sign_value)
+            self.turn_goal_thresh = True
+            self.turn_thresh = True
+        elif self.turn_thresh:
+            print('re-orienting')
+            if self.dist_check < 0.03:
+                self.turn_thresh = False
+        else:
+            self.turn_goal_thresh = False
+            self.goal_add = np.array([0.1,0.0])
+
+        goal_add_transform = self.transform_to_global(self.goal_add,orientation)
+        goal = [curr_pos[0]+goal_add_transform[0],curr_pos[1]+goal_add_transform[1]]
         dist_to_goal = np.sqrt((curr_pos[0]-goal[0])**2+(curr_pos[1]-goal[1])**2)
+        self.dist_check = dist_to_goal
         if dist_to_goal>self.dstar:
             xa,ya = -self.dstar*self.zeta*(np.array(curr_pos)-np.array(goal))/dist_to_goal
         else:
             xa,ya = -self.zeta*(np.array(curr_pos)-np.array(goal))
         xr = []
         yr = []
-        dist_to_obstacle = point.z
-        closest_point = [point.x,point.y]
+        
+
         if dist_to_obstacle<self.qstar:
             # print('ons_in_con')
             coeff = -self.neta*(dist_to_obstacle-self.qstar)/((dist_to_obstacle**4)*self.qstar)
             # print(coeff,'coef')
-            xrg,yrg = coeff*(np.array(curr_pos)-np.array(closest_point))
+            xrg,yrg = coeff*(np.array(curr_pos)-closest_point)
         else:
             xrg,yrg = 0,0
         xr.append(xrg)
@@ -116,7 +158,6 @@ class MoveRobot(Node):
         else:
             self.ang_thresh_active = False
 
-        orientation = self.odom.theta
         print(xu,yu,'xuyu_before')
         lin_vel = self.MAX_LIN*(xu*np.cos(orientation)+yu*np.sin(orientation))
         ang_vel = self.MAX_ANG*(yu*np.cos(orientation)-xu*np.sin(orientation))
@@ -125,10 +166,10 @@ class MoveRobot(Node):
         msg.linear.x = lin_vel
         msg.angular.z = ang_vel
         print(msg.linear.x,msg.angular.z,'linear-angular')
-        if dist_to_goal<self.reached_threshold:
-            print('wp_changed_yayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayaya')
-            self.current_wp_index+=1
-            self.wait_count = 5
+        # if dist_to_goal<self.reached_threshold:
+        #     print('wp_changed_yayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayaya')
+        #     self.current_wp_index+=1
+        #     self.wait_count = 5
 
         self._move_publish.publish(msg)
 
